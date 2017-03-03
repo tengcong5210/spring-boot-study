60b667b95938fb05177886c8c92bb9de4de84256
tengcong5210 <18616231631@126.com>
2017-03-03 23:52:51 +0800

    【springBoot】springboot 1.通过中资获取数据

---
+++ src/main/java/com/tcc/boot/AuthorSeting.java
@@ -0,0 +1,38 @@
+package com.tcc.boot;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by ibm on 2017/3/3.
+ */
+@Component
+@ConfigurationProperties(prefix = "author")
+public class AuthorSeting {
+    private String name;
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "AuthorSeting{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/tcc/boot/SpringBootStudyApplication.java
+++ src/main/java/com/tcc/boot/SpringBootStudyApplication.java
@@ -1,5 +1,7 @@
 package com.tcc.boot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class SpringBootStudyApplication {
+
+	//通过@value注解可以把属性(application.properties)文件中的值注入进来
+	@Value("${book.author}")
+	private String bookAuthor;
+	@Value("${book.name}")
+	private String bookName;
+	@Autowired
+	private AuthorSeting authorSeting;
+
 	@RequestMapping("/")
 	public String index(){
-		return "Hello Spring boot";
+		return "Hello Spring boot,"+bookAuthor+";"+bookName+";authorName:"+authorSeting.getName()+";age:"+authorSeting.getAge();
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootStudyApplication.class, args);
